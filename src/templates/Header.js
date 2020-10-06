2b7c7af3898bb0d4497a3c7e8b5dfa9c4f4a17c0
Hector Rubi <hector.rubi.garcia@outlook.com>
2020-10-05 20:53:24 -0500

    Change home url, this is only for Production

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -2,7 +2,7 @@ const Header = () => {
   const view = `
     <div class="Header-main">
       <div class="Header-logo">
-        <a href="/">
+        <a href="/Rick-morty-spa/">
           <img src="https://partner.redbubble.com/partnership-portal/client/eab637076b22c782edf0015a0ec3c686.png" alt="Logo">
         </a>
       </div>
